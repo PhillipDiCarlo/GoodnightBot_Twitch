ec61a32a4d2e50a2f69a7fa7e174510a5c954121
PhillipDiCarlo <34153910+PhillipDiCarlo@users.noreply.github.com>
2022-08-03 00:33:02 -0400

    'added rand num gen to send msgs more rand'

--- server.js
+++ server.js
@@ -6,13 +6,29 @@ require("dotenv").config();
 
 const tmi = require('tmi.js');
 
+
+function wait(ms)
+{
+    var d = new Date();
+    var d2 = null;
+    do { d2 = new Date(); }
+    while(d2-d < ms);
+}
+
+function randomIntFromInterval(min, max) { // min and max included 
+	x = Math.floor(Math.random() * (max - min + 1) + min)
+	return x * 1000
+
+  }
+
+
 const client = new tmi.Client({
 	options: { debug: true },
 	identity: {
 		username: process.env.TWITCH_BOT_USERNAME,
 		password: process.env.TWITCH_OAUTH_TOKEN
 	},
-	channels: [ 'Italiandogs', 'mizkif' ]
+	channels: [ 'Italiandogs', 'Mizkif']
 });
 
 
@@ -23,7 +39,13 @@ client.on('message', (channel, tags, message, self) => {
 	if(self) return;
 
     //tell chat gn
-	if(process.env.GN_MESSAGES.includes(message.toLowerCase())) {
+	if(message.toLowerCase() === 'gn chat' 
+	|| message.toLowerCase() === 'going to bed' 
+	|| message.toLowerCase() === 'blacklivesmatter c ya chat' 
+	|| message.toLowerCase() === 'BlackLivesMatter gn chat' 
+	|| message.toLowerCase() === 'gn chat pepel'
+	|| message.toLowerCase() === 'off to bed' ) {
+		wait(randomIntFromInterval(5, 15));
 		client.say(channel, `gn ${tags.username} pepeL`);
 	 }
 
